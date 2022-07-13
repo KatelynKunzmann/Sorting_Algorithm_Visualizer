8a8d5f5855604612bb13918420accffdc52f9e2e
Katelyn Kunzmann <katelynk44@gmail.com>
2022-07-13 13:36:07 -0400

    Added insertion sort

--- sort_algo_visualizer.py
+++ sort_algo_visualizer.py
@@ -9,6 +9,8 @@ class DrawInformation:
     GREEN = 0, 255, 0
     RED = 255, 0, 0
     GRAY = 128, 128, 128
+    PURPLE = 131, 22, 245
+    YELLOW = 241, 248, 4
     BG_COLOR = WHITE
 
     GRADIENTS = [
@@ -41,15 +43,18 @@ class DrawInformation:
         self.start_x = self.SIDE_PAD // 2
 
 
-def draw(draw_info):
+def draw(draw_info, algo_name, ascending):
     draw_info.window.fill(draw_info.BG_COLOR)
+
+    title = draw_info.LARGE_FONT.render(f"{algo_name} - {'Ascending' if ascending else 'Descending'}", 1, draw_info.PURPLE)
+    draw_info.window.blit(title, (draw_info.width/2 - title.get_width()/2, 5))
 
     controls = draw_info.FONT.render("R - Reset | SPACEBAR - Start Sorting | A - Ascending | D - Descending", 1, draw_info.BLACK)
     # Must subtract the width of the text/2 by the width of the screen/2 to land in the proper x coordinate for the text
-    draw_info.window.blit(controls, (draw_info.width/2 - controls.get_width()/2, 5))
+    draw_info.window.blit(controls, (draw_info.width/2 - controls.get_width()/2, 45))
 
     sorting = draw_info.FONT.render("I - Insertion Sort | B - Bubble Sort | M - Merge Sort | Q - Quick Sort", 1, draw_info.BLACK)
-    draw_info.window.blit(sorting, (draw_info.width/2 - sorting.get_width()/2, 35))
+    draw_info.window.blit(sorting, (draw_info.width/2 - sorting.get_width()/2, 75))
 
     draw_list(draw_info)
     pygame.display.update()
@@ -96,10 +101,33 @@ def bubble_sort(draw_info, ascending=True):
 
             if (num1 > num2 and ascending) or (num1 < num2 and not ascending):
                 lst[j], lst[j+1] = lst[j+1], lst[j]
-                #draw_list()
-                # Yield keyword Pauses the execution sort of halfway through and stores the current state of the function - creates a generator
+                # Yield keyword for whenever you want to draw something
+                # It pauses the execution sort of halfway through and stores the current state of the function - creates a generator
                 # Will allow us to continue using controls like 'exit'
                 draw_list(draw_info, {j: draw_info.GREEN, j+1: draw_info.RED}, True)
+                yield True
+
+    return lst
+
+
+def insertion_sort(draw_info, ascending=True):
+    lst = draw_info.lst
+
+    for i in range(1, len(lst)):
+        current = lst[i]
+
+        while True:
+            ascending_sort = i > 0 and lst[i-1] > current and ascending
+            descending_sort = i > 0 and lst[i-1] < current and not ascending
+
+            if not ascending_sort and not descending_sort:
+                break
+            else:
+                # Swaps
+                lst[i] = lst[i-1]
+                i = i-1
+                lst[i] = current
+                draw_list(draw_info, {i-1: draw_info.GREEN, i: draw_info.RED}, True)
                 yield True
 
     return lst
@@ -126,7 +154,7 @@ def main():
     # Constant loop to keep window active
     while running:
         # Maximum amount of times the loop can run per second (fps)
-        clock.tick(60)
+        clock.tick(80)
 
         if sorting:
             try:
@@ -134,9 +162,9 @@ def main():
             except StopIteration: # Generator is finished
                 sorting = False
         else:
-            draw(draw_info)
+            draw(draw_info, sorting_algo_name, ascending)
 
-        draw(draw_info)
+        draw(draw_info, sorting_algo_name, ascending)
 
         pygame.display.update()
 
@@ -157,6 +185,13 @@ def main():
                 ascending = True
             elif event.key == pygame.K_d and not sorting:
                 ascending = False
+            elif event.key == pygame.K_i and not sorting:
+                sorting_algo = insertion_sort
+                sorting_algo_name = "Insertion Sort"
+            elif event.key == pygame.K_b and not sorting:
+                sorting_algo = bubble_sort
+                sorting_algo_name = "Bubble Sort"
+            
 
         
     pygame.quit()
